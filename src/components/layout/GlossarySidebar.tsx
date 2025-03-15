4f04415f8d7480c985ae41c81f2c79fed8bdb53f
CalsonicSSS <calson.developer@gmail.com>
2025-03-15 19:29:21 -0400

    glossary side nav added

--- src/components/layout/GlossarySidebar.tsx
+++ src/components/layout/GlossarySidebar.tsx
@@ -1,11 +1,10 @@
-'use client';
-
 import { assetPathMode } from '@/utils';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function GlossarySidebar() {
   return (
-    <div className='relative hover:cursor-pointer'>
+    <Link href={'/doc/glossary'} className='relative hover:cursor-pointer'>
       <div className='fixed right-0 top-1/4 bg-[#195b8b] py-[20PX] px-[17px] flex flex-col items-center justify-center shadow-2xl'>
         {/* Info icon */}
         <Image
@@ -20,6 +19,6 @@ export default function GlossarySidebar() {
         {/* Vertical text */}
         <div className='writing-vertical-lr transform rotate-180 font-roboto font-normal text-[20px] leading-[31px] text-white tracking-[2px]'>Glossary</div>
       </div>
-    </div>
+    </Link>
   );
 }
